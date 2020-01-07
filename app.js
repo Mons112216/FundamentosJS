229d23e5ef07363b14b1ea58287a6d58aa8b38d5
Mons112216 <mongarz099@gmail.com>
2020-01-07 13:58:20 -0600

    JS

---
+++ app.js
@@ -0,0 +1,20 @@
+var nombre = 'Fatima' , apellido = 'Garcia'
+
+var nomMay = nombre.toUpperCase()
+var apMin = apellido.toLowerCase()
+
+var primeraLetraNombre = nombre.charAt(0)
+var cantidadLetras = nombre.length
+
+ var nomCompleto = `${nombre.toUpperCase()} ${apellido.toUpperCase()}` 
+
+ var str = nombre.substr(0, 3)
+
+var ultimaLetra = `${nombre.charAt(nombre.length-1)}`
+// var edad = 28
+
+// edad = '28 años'
+
+// console.log('Hola '+ nombre + ' ' + apellido)
+// console.log('tengo '+ edad)
+
